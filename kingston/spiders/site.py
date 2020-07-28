ec4d5e1bcf63fb6803f1dd1d8102a0d335f7a5e3
raspi <pekka.jarvinen@gmail.com>
2020-07-28 19:27:33 +0300

    added comments

--- kingston/spiders/site.py
+++ kingston/spiders/site.py
@@ -32,6 +32,9 @@ class ManufacturerSpider(scrapy.Spider):
         self.start_urls = ['https://www.kingston.com/en']
 
     def parse(self, response: scrapy.http.Response):
+        """
+        POST to get a list of manufacturer's motherboard models
+        """
         yield scrapy.FormRequest(
             'https://www.kingston.com/en/memorysearch/ajax/getlines',
             callback=self.POST_parse_models,
@@ -46,6 +49,9 @@ class ManufacturerSpider(scrapy.Spider):
         )
 
     def POST_parse_models(self, response: scrapy.http.Response):
+        """
+        List models and meta information
+        """
         for model in json.loads(response.body):
             yield scrapy.FormRequest(
                 'https://www.kingston.com/en/memorysearch/ajax/getmodels',
@@ -63,6 +69,9 @@ class ManufacturerSpider(scrapy.Spider):
             )
 
     def POST_parse_model(self, response: scrapy.http.Response):
+        """
+        Get model's meta information
+        """
         data = json.loads(response.body)[0]
         yield scrapy.Request(
             f"https://www.kingston.com/en/memory/search?model={data['Value']}&mfr={self.manufacturer}",
@@ -76,7 +85,7 @@ class ManufacturerSpider(scrapy.Spider):
 
     def parse_memory(self, response: scrapy.http.Response):
         """
-        Get compatible memory modules
+        Get compatible memory modules for certain motherboard
         """
 
         prodinfo = {}
@@ -94,13 +103,19 @@ class ManufacturerSpider(scrapy.Spider):
         })
 
         for section in response.xpath("//section[contains(@class, 'section-product_gallery')][@id]"):
+            # Iterate each section which lists compatible products such as memory, SSDs, etc..
+
+            # title is for example "ValueRAM Server Premier"
             title = section.xpath(".//h2/text()").get().strip()
             if "RAM" not in title:
+                # We're not interested in SSDs and such
                 continue
 
             compatible_modules[title] = []
 
             for memmodule in section.xpath("./ul[contains(@class, 'c-productGallery')]/li"):
+                # List each memory module
+
                 name = memmodule.xpath("./@data-pn").get()
 
                 moduleinfo = {
@@ -108,20 +123,25 @@ class ManufacturerSpider(scrapy.Spider):
                 }
 
                 for infoline in memmodule.xpath(".//div[@class='c-productCard__body__details']/ul/li"):
+                    # List each memory module's specification line
+
                     txt = "".join(infoline.xpath(".//text()").getall()).strip()
 
                     if txt == 'PCN':
                         # Product change notify, ignore
                         continue
                     elif 'Spec Sheet PDF' in txt:
+                        # PDF link
                         moduleinfo[txt] = infoline.xpath(".//a/@href").get()
                     else:
                         if ':' not in txt:
                             continue
 
+                        # for example "Part Number: KVR24N17D8/16"
                         k, v = txt.split(": ", 1)
 
                         if 'Specs' in k:
+                            # for example "DDR4, 2400MHz, Non-ECC, CL17, 1.2V, Unbuffered, DIMM"
                             v = v.split(", ")
 
                         moduleinfo[k] = v
